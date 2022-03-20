abf905666fbee062309b7a4165b11225140908b0
Bao Tran <tieubaoca98@gmail.com>
2022-03-20 08:13:42 +0700

    tmp

--- pallets/kitties/src/benchmarking.rs
+++ pallets/kitties/src/benchmarking.rs
@@ -5,7 +5,9 @@ use super::*;
 #[allow(unused)]
 use crate::Pallet as Kitties;
 use frame_benchmarking::{account, benchmarks, whitelisted_caller};
+use frame_support::traits::tokens::fungible::Unbalanced;
 use frame_system::RawOrigin;
+use pallet_balances::Pallet as Balances;
 benchmarks! {
 	create_kitty {
 		let s in 0 .. 100;
@@ -26,6 +28,7 @@ benchmarks! {
 		let to: T::AccountId = account("to", 2u32, 2u32);
 		let _ = Kitties::<T>::create_kitty(RawOrigin::Signed(caller.clone()).into());
 		let kitty_hashes = Kitties::<T>::kitties_owned(caller.clone());
+		Balances::<T>::set_balance( RawOrigin::Root.into(), to.clone(), s.into(), 0);
 	} : _(RawOrigin::Signed(caller), to, kitty_hashes[0])
 	// buy_kitty{
 	// 	let s in 0 .. 10000000;
@@ -39,4 +42,5 @@ benchmarks! {
 	// breed_kitty{}: _(RawOrigin::Signed(caller), kitty_ha)
 
 	impl_benchmark_test_suite!(Kitties, crate::mock::new_test_ext(), crate::mock::Test);
+	// impl_benchmark_test_suite!(Balances, crate::mock::new_test_ext(), crate::mock::Test);
 }
